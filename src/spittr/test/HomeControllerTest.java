7cb61d4dd9f284ad8199dd12c79070c92100191f
Pythephant <jason_wujiakun@yeah.net>
2018-09-14 15:31:33 +0800

    complished the SpringTomcatEclipse configuration, and exclude the WEB-INF.lib and WEB-INF.classes folder to the repository

---
+++ src/spittr/test/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package spittr.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import spittr.web.HomeController;
+
+public class HomeControllerTest {
+	@Test
+	public void testHomePage() {
+		HomeController controller = new HomeController();
+		assertEquals("home", controller.home());
+	}
+
+	@Test
+	public void testHomePageWithMock() throws Exception {
+		HomeController controller = new HomeController();
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(MockMvcResultMatchers.view().name("home"));
+	}
+}
