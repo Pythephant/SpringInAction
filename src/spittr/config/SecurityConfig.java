0c1649b6f8818d032f51071fd4f387f3de048b56
Jason_Wu <jason_wujiakun@yeah.net>
2018-11-06 07:27:16 +0800

    ...

--- src/spittr/config/SecurityConfig.java
+++ src/spittr/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,7 +19,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.formLogin().and().logout().logoutSuccessUrl("/");
+		http.formLogin().and().logout().logoutSuccessUrl("/").and().authorizeRequests().antMatchers("/spitter/**")
+				.access("hasRole(USER)").antMatchers(HttpMethod.POST, "/spittles").hasRole("USER").anyRequest()
+				.permitAll().and().requiresChannel().antMatchers("/spitter/registerForm").requiresSecure();
 	}
 
 	@Override
